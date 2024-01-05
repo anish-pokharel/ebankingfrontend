e0b48ef03ada87fb80d9923bc79bb8870782c5d9
Anish Pokharel <anish.pokharel.710.ap@gmail.com>
2024-01-05 15:24:39 +0545

    transfer

--- src/app/components/dashboard-pages/transfer/transfer.component.ts
+++ src/app/components/dashboard-pages/transfer/transfer.component.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormGroupDirective, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-transfer',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,FormsModule,ReactiveFormsModule],
   templateUrl: './transfer.component.html',
   styleUrl: './transfer.component.css'
 })
-export class TransferComponent {
+export class TransferComponent implements OnInit{
+formData!: FormGroup;
+constructor( private fb:FormBuilder){}
+  ngOnInit(): void {
+    this,this.transferMoney()
+  
+}
+sendMoney(){
+console.log("button is clicked");
+if(this.formData.valid){
+  const transferData= this.formData.value;
+  console.log(transferData);
+}
+else{}
+}
+
+transferMoney(){
+  this.formData=this.fb.group({
+    beneficiary:['',Validators.required],
+    amount:['',Validators.required],
+    date:['',Validators.required],
+    transferpurpose:['',Validators.required],
+    beneficiarymname:['',Validators.required],
+    beneficiaryemail:['',Validators.required],
+    beneficiaryinfo:['',Validators.required]
+
+  })
+}
 
 }
